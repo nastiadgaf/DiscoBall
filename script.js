ac018381438541399933b17c698584a3190d9ec5
nastiadgaf <nastiadgaf@gmail.com>
2022-01-31 13:21:21 +0200

    Add discoBall
    Discoball changes it's position, background-color, border with interval

---
+++ script.js
@@ -0,0 +1,34 @@
+const ball = $('#ball');
+let timer = setInterval(function(){
+    let maxWidth = $(window).width();
+    let maxHeight = $(window).height();
+    let height = `${getRandomArbitrary(1, maxHeight - 100)}px`;
+    let width = `${getRandomArbitrary(1, maxWidth - 100)}px`;
+    let borderColor = getRandomColor();
+   // let boxShadow = getRandomShadowColor();
+    ball.animate({
+        top: height,
+        left: width,
+        backgroundColor: `#${getRandomColor()}`,
+        borderTopColor: `#${borderColor}`,
+        borderBottomColor: `#${borderColor}`,
+        borderRightColor: `#${borderColor}`,
+        borderLeftColor: `#${borderColor}`,
+        boxShadowColor: `0px 0px 16px 24px rgba(${getRandomShadowColor()}, 0.84)`
+    }, 1000)
+}, 1000);
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomColor() {
+    return Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function getRandomShadowColor() {
+    const getRandomRGB = () => Math.floor(Math.random() * 256);
+    let shadowColor = `${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()}`;
+    console.log(shadowColor)
+    return shadowColor;
+}
